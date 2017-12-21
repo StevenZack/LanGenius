0141ac3ca29c97a3f727a7e8c475ec91d655dbd2
StevenZack <stevenzack@qq.com>
2017-12-21 22:54:25 +0800

    Just a backup

--- langenius.go
+++ langenius.go
@@ -48,3 +48,49 @@ func getFileName(s string) string {
 	}
 	return ""
 }
+func GetIP() string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	var strs []string
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		for _, addr := range addrs {
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip := v.IP
+				strs = append(strs, ip.String())
+			case *net.IPAddr:
+				// ip := v.IP
+				// strs = append(strs, ip.String())
+			}
+		}
+	}
+	for _, v := range strs {
+		if v[:3] == "10." {
+			return v
+		}
+	}
+	for _, v := range strs {
+		if v[:4] == "172." {
+			return v
+		}
+	}
+	for _, v := range strs {
+		if v[:8] == "192.168." {
+			return v
+		}
+	}
+	for _, v := range strs {
+		if v != "127.0.0.1" && v != "::1" {
+			return v
+		}
+	}
+	return strs[0]
+}
